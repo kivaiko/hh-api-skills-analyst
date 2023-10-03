5389807db845fc60a99ede5d6fa9fc86f8fb47b4
Alex Kivaiko <alexagree1@gmail.com>
2023-10-03 16:37:11 +0400

    Fix for github

--- main.py
+++ main.py
@@ -1,81 +1,90 @@
 import requests
 import json
 import re
+import bleach
 from collections import Counter
-
-CLIENT_ID = "GT11P1DLO4KP6OTSN0KQOQ4OA99KLPFAMRD4BJUKQ2RQS3SIA4COQEE738PRA5I7"
-CLIENT_SECRET = "VQ2JNMG0579HOSRVD6M9B4TQLET5MN52M4R842QRVIETUNU549MI5DRHNKLFS9Q8"
-ACCESS_TOKEN = "APPLPBBQB1CDN4DHJAPCSTR92M73395AU7RPU4K8LI6NSDDSUCFF87049KTUC9DR"
-#  "token_type": "bearer"
-
-headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'User-Agent': 'skill rating (alexagree1@gmail.com)',
-    'Authorization': 'Bearer APPLPBBQB1CDN4DHJAPCSTR92M73395AU7RPU4K8LI6NSDDSUCFF87049KTUC9DR',
-    'Content-Type': 'application/json'
-}
+from filter import words_filter
+from conf import HEADERS
 
 
-search_url = 'https://hh.ru/search/vacancy?employment=part&experience=between3And6&search_field=name&search_field=company_name&search_field=description&text=python+backend&ored_clusters=true&enable_snippets=true&L_save_area=true'
+""" Поисковы запрос на HH.Важно! Иногда hh добавляет в запрос параметр "&salary=",
+нужно убрать его иначе api не будет отдавать данные
+"""
+search_url = 'https://hh.ru/search/vacancy?text=frontend&ored_clusters=true&enable_snippets=true&area=2758&hhtmFrom=vacancy_search_list'
+
+
+""" Сколько оставь слов в итоговой выборке """
+values_in_top = 100
 
 
 def get_link(url):
+    print('Start – get_link')
     url = url[29:]
-    link = f'https://api.hh.ru/vacancies?{url}&per_page=100'
-    return link
+    search_link = f'https://api.hh.ru/vacancies?{url}&per_page=100'
+    return search_link
 
 
 def get_vacancies_id(url):
-    vacancies = []
-    response = requests.get(url, headers=headers)
+    print('Start – get_vacancies_id')
+    vacancies_ids = []
+    response = requests.get(url, headers=HEADERS)
     data = json.loads(response.text)
     pages = data['pages']
     for page in range(pages):
-        response = requests.get(url, headers=headers, params={'page': page})
+        response = requests.get(url, headers=HEADERS, params={'page': page})
         data = json.loads(response.text)
         clear_data = data['items']
         for i in clear_data:
-            vacancies.append(i['id'])
-    return vacancies
+            vacancies_ids.append(i['id'])
+    return vacancies_ids
 
 
-def get_skills(ids):
-    skills = []
-    for vacancy_id in ids:
-        response = requests.get('https://api.hh.ru/vacancies/' + vacancy_id, headers=headers)
-        data = json.loads(response.text)
-        for skill in data['key_skills']:
-            skills.append(skill['name'])
-    return skills
+def get_skills_from_id(data):
+    skills_for_id = []
+    for skill in data:
+        skills_for_id.append(skill['name'])
+    return skills_for_id
 
 
-def get_keywords(ids):
+def get_keywords_from_id(data):
     vacancies_description = ''
-    for vacancy_id in ids:
-        response = requests.get('https://api.hh.ru/vacancies/' + vacancy_id, headers=headers)
-        data = json.loads(response.text)
-        vacancies_description += data['description']
-        words = clean(vacancies_description)
-    return words
-
-
-def clean(txt):
-    words_list = []
+    txt = bleach.clean(data, tags=[], strip=True)
+    txt = re.findall(r'[A-Za-z0-9]+', txt)
+    txt = ' '.join(txt)
     txt = txt.lower()
-    cleaned_txt = re.sub(r'(\<(/?[^>]+)>)|[)(?«1.2•,:+!—;»3–⁃-]|[а-яё]|backend|&quot|(\\)', '', txt)
-    for word in cleaned_txt.split():
-        words_list.append(word)
+    vacancies_description += txt
+    words_list = clean(vacancies_description)
     return words_list
 
 
-link = get_link(search_url)
-vacancies_ids = get_vacancies_id(link)
-keywords = get_keywords(vacancies_ids)
-skills = get_skills(vacancies_ids)
+def get_data_from_vacancies_id(ids):
+    print('Start – get_data_from_vacancies_id')
+    skills_list = []
+    keywords_list = []
+    for vacancy_id in ids:
+        print(vacancy_id)
+        response = requests.get('https://api.hh.ru/vacancies/' + vacancy_id, headers=HEADERS)
+        data = json.loads(response.text)
+        skills_list += get_skills_from_id(data['key_skills'])
+        keywords_list += get_keywords_from_id(data['description'])
+    return skills_list, keywords_list
 
 
-print(Counter(keywords).most_common(100))
+def clean(txt):
+    all_words = []
+    for word in txt.split():
+        all_words.append(word)
+    new_list = [item for item in all_words if item not in words_filter]
+    return new_list
 
 
+def count_words(url):
+    link = get_link(url)
+    vacancies_ids = get_vacancies_id(link)
+    skills, keywords = get_data_from_vacancies_id(vacancies_ids)
+    print(Counter(skills).most_common(values_in_top))
+    print(' ')
+    print(Counter(keywords).most_common(values_in_top))
 
 
+count_words(search_url)
